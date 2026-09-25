97f16fccd945f4e817cabe24e71d1d2eed04c48f
Abhishek Kumar <bika2413@gmail.com>
2026-09-25 16:45:20 +0530

    webhook added

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         console.log("Database Connected");
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/back_removal`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/back removal`)
 
 }
 
--- server/server.js
+++ server/server.js
@@ -2,17 +2,19 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 // App Config
 const PORT = process.env.PORT || 4000
 const app = express()
 await connectDB()
 
-// Initialize Middlewares
+// Intialize Middlewares
 app.use(express.json())
 app.use(cors())
 
-// API routes
-app.get('/', (req, res) => res.send("API Working"))
+// API Routes
+app.get('/',(req,res)=> res.send("API Working"))
+app.use('/api/user',userRouter)
 
-app.listen(PORT, () => console.log("Server Running on port " + PORT))
+app.listen(PORT, ()=> console.log("Server Running on port "+PORT))
